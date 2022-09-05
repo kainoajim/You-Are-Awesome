1fd703c2e48138fc24e168989aa48f4a149ae8c7
Kainoa Jim <jimkb@bc.edu>
2022-09-05 14:18:46 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Kainoa Jim on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
