a821561930739c109b0c8ae24eae4f24f9379846
Kainoa Jim <jimkb@bc.edu>
2022-09-12 16:06:40 -0400

    Finish Section 2

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     
     var body: some View {
         
         VStack {
-            
-            Spacer()
             
             Text(messageString)
                 .font(.largeTitle)
@@ -24,35 +27,61 @@ struct ContentView: View {
                 .foregroundColor(.red)
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
-            //                    .border(.orange, width: 1)
                 .padding()
             
-            //                Spacer()
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
+            
+            
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Spacer()
-                
-                
-                
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
-            }
-            //            .border(.purple, width: 5)
-            .padding()
-            .tint(Color("Vermillion"))
             
+            Spacer()
+            
+            Button("Show Message") {
+
+                
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "Fabulous? That's You!",
+                                "You Are Built Different!",
+                                "You Can't Be Stopped!",
+                                "You Da Best!"]
+                
+                
+                var messageNumber: Int
+                
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
+                } while messageNumber == lastMessageNumber
+                
+                messageString = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+//                imageName = "image\(Int.random(in: 0...9))"
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+
+                
+            }
+            .buttonStyle(.borderedProminent)
             
         }
+        .padding()
+        
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
